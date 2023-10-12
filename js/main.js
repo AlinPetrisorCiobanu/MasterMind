d9213db373936984f4ef76a0e318e3f76360d628
Rumanito12 <ady_9697@yahoo.com>
2023-10-12 19:35:16 +0200

    creación del colorPicker y diseño de la pagina de selector de colores

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
+const colorUno=document.getElementById("colorUno")
+const colorDos=document.getElementById("colorDos")
+const colorTres=document.getElementById("colorTres")
+const colorCuatro=document.getElementById("colorCuatro")
+const colorSeleccionadoUno=document.getElementById("colorSeleccionadoUno")
+const colorSeleccionadoDos=document.getElementById("colorSeleccionadoDos")
+const colorSeleccionadoTres=document.getElementById("colorSeleccionadoTres")
+const colorSeleccionadoCuatro=document.getElementById("colorSeleccionadoCuatro")
+colorSeleccionadoUno.style.background=colorUno.value
+colorSeleccionadoDos.style.background=colorDos.value
+colorSeleccionadoTres.style.background=colorTres.value
+colorSeleccionadoCuatro.style.background=colorCuatro.value
 
+    
+
+colorUno.addEventListener('input',()=>{
+    colorSeleccionadoUno.style.background=colorUno.value
+})
+colorDos.addEventListener('input',()=>{
+    colorSeleccionadoDos.style.background=colorDos.value
+})
+colorTres.addEventListener('input',()=>{
+    colorSeleccionadoTres.style.background=colorTres.value
+})
+colorCuatro.addEventListener('input',()=>{
+    colorSeleccionadoCuatro.style.background=colorCuatro.value
+})
