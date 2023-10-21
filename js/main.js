95d4903d4bb000f636e94f5b65892ea334c64c4a
Rumanito12 <ady_9697@yahoo.com>
2023-10-21 16:14:24 +0200

    Js añadido contador de numero suma y resta

--- js/main.js
+++ js/main.js
@@ -1,91 +1,122 @@
-    //Saque de todos los datos guardados anteriromente -->
-const nombreJugador=sessionStorage.getItem("nombreJugador")
-const dificultad=sessionStorage.getItem("dificultad")
-const nrDeColores=sessionStorage.getItem("colores")
-const nrDeIntentos=sessionStorage.getItem("intentos")
+//Saque de todos los datos guardados anteriromente -->
+const nombreJugador = sessionStorage.getItem("nombreJugador")
+const dificultad = sessionStorage.getItem("dificultad")
+const nrDeColores = sessionStorage.getItem("colores")
+const nrDeIntentos = sessionStorage.getItem("intentos")
 const paletaDeColores = [];
 const sacarColores = (colores) => {
-    for ( let i = 0 ; i<colores ; i++){
-        paletaDeColores[i]=sessionStorage.getItem("color"+i)
+    for (let i = 0; i < colores; i++) {
+        paletaDeColores[i] = sessionStorage.getItem("color" + i)
     }
 }
 sacarColores(nrDeColores)
-    //Asignacion de datos en la web
-document.querySelector('.nombreJugador').innerHTML=nombreJugador
-document.querySelector('.nombreDificutad').innerHTML=`Level : ${dificultad}`
+//Asignacion de datos en la web
+document.querySelector('.nombreJugador').innerHTML = nombreJugador
+document.querySelector('.nombreDificutad').innerHTML = `Level : ${dificultad}`
 const creadorDeDivsInteractivos = (vueltas) => {
-    for( let i = 0 ; i < vueltas ; i++){
+    for (let i = 0; i < vueltas; i++) {
         document.querySelector(".incorporacionDeDivsInteractivos").innerHTML += `<div class="col-12">
                                                                             <div class="row justify-content-center">
-                                                                                <div id="divInteractivo${i}" class="col-11 divClickGetColorGame" onclick="selector(${i})"></div>
+                                                                                <div id="divInteractivo${i}" class="col-11 divClickGetColorGame" onclick="selector(${i},contador)"></div>
                                                                             </div>
                                                                         </div>
-                                                                        <div class="col-12 espaciadorPequenio"></div>`        
-    } 
+                                                                        <div class="col-12 espaciadorPequenio"></div>`
+    }
 }
 creadorDeDivsInteractivos(nrDeColores)
 const asignarColoresDeLaPaleta = (color) => {
-    for ( let i = 0 ; i<color.length ; i++){
-        document.getElementById(`divInteractivo${i}`).style.backgroundColor=color[i]   
+    for (let i = 0; i < color.length; i++) {
+        document.getElementById(`divInteractivo${i}`).style.backgroundColor = color[i]
     }
 }
 asignarColoresDeLaPaleta(paletaDeColores)
 const creadorDeDivsAsignados = (vueltas) => {
-    for( let i = 0 ; i < vueltas ; i++){
+    for (let i = 0; i < vueltas; i++) {
         document.querySelector(".creadorDeDivsAsignados").innerHTML += `
                                 <div class="col-6 col-lg-3 justify-content-center align-items-center">
                                     <div class="row justify-content-center">
                                         <div id="columna${i}" class="col-11 divsColoresSelector">                                            
                                         </div>
                                     </div>
-                                </div>` 
-                                if((i==3)||(i==7)||(i==11)||(i==15)||(i==19)||(i==23)||(i==27)||(i==31)||(i==35)){
-                                    document.querySelector(".creadorDeDivsAsignados").innerHTML +=`
+                                </div>`
+        if ((i == 3) || (i == 7) || (i == 11) || (i == 15) || (i == 19) || (i == 23) || (i == 27) || (i == 31) || (i == 35)) {
+            document.querySelector(".creadorDeDivsAsignados").innerHTML += `
                                     <div class="col-12 espaciadorPequenio"></div>`
-                                }       
-    } 
-    
-}
-creadorDeDivsAsignados(nrDeIntentos*4)
-        //formación aleatoria de Numeros
-const arrayNumerosAleatorios=[];
-const numerosAleatorios=(num)=>{
-    for(let i=0;i<=3;i++){
-        arrayNumerosAleatorios.push(Math.floor(Math.random()*num))
+        }
     }
-    //console.log(arrayNumerosAleatorios)
+
+}
+const arrayNumerosAleatorios = [];
+creadorDeDivsAsignados(nrDeIntentos * 4)
+//formación aleatoria de Numeros
+
+const numerosAleatorios = (num) => {
+    /*
+    var lista = [1,2,3,4,5,6,7,8,9];
+lista = lista.sort(function() {return Math.random() - 0.5});
+document.write(lista)
+    */
+    let numeros = [0,1,2,3,4,5,6];
+    for (let i = 0; i <= 3; i++) {
+        arrayNumerosAleatorios.push(Math.floor(Math.random() * num))
+    }
+   console.log(arrayNumerosAleatorios)
 }
 numerosAleatorios(nrDeColores)
 
+class Contador {
+    valor = 0;
+    constructor() {
+        this.valor = 0
+    }
+    suma() {
+        this.valor++;
+    }
+    resta() {
+        this.valor--;
+    }
+}
+let contador = new Contador()
+
+const numerosComparacion = []
+
+const selector = (num, numContador) => {
+    if ((numContador.valor >= 0) && (numContador.valor <= 3)) {
+        ejecucion(num, numContador.valor)
+        contador.suma()
+    }
+}
+
+const borrar = (nr, numContador) => {
+    if ((numContador.valor >= 1)) {
+        nr = 'numeroOculto'
+        contador.resta()
+        ejecucion(nr, numContador.valor)
+
+    }
+}
 
 
-let c=0;
-const numerosComparacion=[]
-    const selector=(num)=>{
-           const columna = document.getElementById(`columna${c}`)
-           columna.style.background=paletaDeColores[num]
-           numerosComparacion.push(num)
-            c++;                       
-                }  
+const ejecucion = (num, valor) => {
+    if ((numerosComparacion.length < 4) || (num == 'numeroOculto')) {
+        const columna = document.getElementById(`columna${valor}`)
 
-                const verificacion=()=>{
-                    // for (let i=0;i<numerosComparacion.length;i++){
-                         if(numerosComparacion[0]==arrayNumerosAleatorios[0]){
-                             if(numerosComparacion[1]==arrayNumerosAleatorios[1]){
-                                 if(numerosComparacion[2]==arrayNumerosAleatorios[2]){
-                                     if(numerosComparacion[3]==arrayNumerosAleatorios[3]){
-                                         alert('PEroooo que suerte')
-                                     }else{
-                                         alert('JODEEEEER')
-                                     }
-                                 }else{
-                                     alert('Faltaba muy poco')
-                                 }
-                             }else{
-                                 alert('casi')
-                             }
-                         }else{
-                             alert('muy lejos')
-                         }
-                   //  }
-                 }
+        if (num >= 0) {
+            columna.style.background = paletaDeColores[num]
+            numerosComparacion.push(num)
+        }
+        else if (num == 'numeroOculto') {
+            columna.style.background = '#ffffff00'
+            numerosComparacion.shift()
+        }
+    }
+
+    console.log(numerosComparacion)
+}
+const verificacion = (array1, array2) => {
+    console.log(array1)
+    console.log(array2)
+    if ((array1[0] == array2[0]) && (array1[1] == array2[1]) && (array1[2] == array2[2]) && (array1[3] == array2[3])) return console.log('perfecto')
+   
+
+}
