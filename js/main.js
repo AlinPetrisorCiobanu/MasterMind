8b2c2ded500e90ecf96859cb90afaf5d1d245ee7
Rumanito12 <ady_9697@yahoo.com>
2023-10-21 21:14:46 +0200

    Js pequeño avance y areglo de bugs

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ const creadorDeDivsInteractivos = (vueltas) => {
         document.querySelector(".incorporacionDeDivsInteractivos").innerHTML += `
         <div class="col-12">
             <div class="row justify-content-center">
-                <div id="divInteractivo${i}" class="col-11 divClickGetColorGame" onclick="selector(${i},contadorColumna)"></div>
+                <div id="divInteractivo${i}" class="col-11 divClickGetColorGame" onclick="selector(${i},contadorColumna,contadorFila)"></div>
             </div>
         </div>
         <div class="col-12 espaciadorPequenio"></div>`
@@ -78,40 +78,72 @@ class ContadorColumna {
 }
 
 class ContadorFila { 
-    valor = 0;
+    min = 0;
+    max =4;
     constructor(){
-        this.valor=0;
+        this.min=0;
+        this.max=4;
     }
-    suma(){
-        this.valor = this.valor+4
+    suma1(){
+        this.min = this.min+4
+    }
+    suma2(){
+        this.max = this.max+4
+    }
+}
+class SelectorDeColumnaComprobacion {
+    colUno = 0;
+    colDos = 1;
+    colTres = 2;
+    colCuatro = 3;
+    constructor(){
+        this.colUno =0
+        this.colDos =1
+        this.colTres =2
+        this.colCuatro =3
+    }
+    suma1(){
+        this.colUno=this.colUno+4;       
+    }
+    suma2(){
+        this.colDos=this.colDos+4;
+    }
+    suma3(){
+        this.colTres=this.colTres+4;
+    }
+    suma4(){
+        this.colCuatro=this.colCuatro+4;
     }
 }
 
-
 let contadorColumna = new ContadorColumna()
 let contadorFila = new ContadorFila()
+let selectorDeColumna = new SelectorDeColumnaComprobacion()
+
+
+console.log(selectorDeColumna)
 
 const numerosComparacion = []
 
-const selector = (num, numContador) => {
-    if ((numContador.valor >= 0) && (numContador.valor <= 3)) {
-        ejecucion(num, numContador.valor)
+const selector = (num, numContador,fila) => {
+    if ((numContador.valor >= 0) && (numContador.valor < fila.max)) {
+        ejecucion(num, numContador.valor,fila.max)
         contadorColumna.suma()
     }
 }
 
 const borrar = (nr, numContador) => {
-    if ((numContador.valor >= 1)) {
+    if ((numContador.valor >= 1)&&(numContador.valor>contadorFila.min)) {
         nr = 'numeroOculto'
         contadorColumna.resta()
         ejecucion(nr, numContador.valor)
-
     }
 }
 
 
-const ejecucion = (num, valor) => {
-    if ((numerosComparacion.length < 4) || (num == 'numeroOculto')) {
+const ejecucion = (num, valor,fila) => {
+    console.log(valor)
+    if ((numerosComparacion.length < fila) || (num == 'numeroOculto')) {
         const columna = document.getElementById(`columna${valor}`)
 
         if (num >= 0) {
@@ -126,71 +158,92 @@ const ejecucion = (num, valor) => {
         }
     }
 }
-const verificacion = (array1, array2) => {
-    // console.log(array1)
-    // console.log(array2)
+const verificacion = (array1, array2,column) => {
+    console.log(array1)
+    console.log(array2)
+    console.log(column.colUno)
+    console.log(column.colDos)
+    console.log(column.colTres)
+    console.log(column.colCuatro)
+
+
+
 
     if (array1.length === 4) {
-
-        if (array1[0] === array2[0]) {
-            const columna = document.getElementById(`columna0`)
-            columna.style.borderWidth = `${6}px`
-            columna.style.borderColor = `#44ff00`
-          
-        }
-        if (array1[1] === array2[1]) {
-            const columna = document.getElementById(`columna1`)
-            columna.style.borderWidth = `${6}px`
-            columna.style.borderColor = `#44ff00`
-            
-        }
-        if (array1[2] === array2[2]) {
-            const columna = document.getElementById(`columna2`)
-            columna.style.borderWidth = `${6}px`
-            columna.style.borderColor = `#44ff00`
-            
-        }
-        if (array1[3] === array2[3]) {
-            const columna = document.getElementById(`columna3`)
-            columna.style.borderWidth = `${6}px`
-            columna.style.borderColor = `#44ff00`
-         
-        }
-        
-
-
         for (let i = 0; i < array1.length; i++) {
             if ((array1[i] != array2[i]) && (array1[i]) == array2[0]) {
-                const columna = document.getElementById(`columna${i}`)
+                const columna = document.getElementById(`columna${column.colUno}`)
                 columna.style.borderWidth = `${4}px`
                 columna.style.borderColor = `#ffff00`
             }
             else if ((array1[i] != array2[i]) && (array1[i]) == array2[1]) {
-                const columna = document.getElementById(`columna${i}`)
+                const columna = document.getElementById(`columna${column.colDos}`)
                 columna.style.borderWidth = `${4}px`
                 columna.style.borderColor = `#ffff00`
             }
             else if ((array1[i] != array2[i]) && (array1[i]) == array2[2]) {
-                const columna = document.getElementById(`columna${i}`)
+                const columna = document.getElementById(`columna${column.colTres}`)
                 columna.style.borderWidth = `${4}px`
                 columna.style.borderColor = `#ffff00`
             }
             else if ((array1[i] != array2[i]) && (array1[i]) == array2[3]) {
-                const columna = document.getElementById(`columna${i}`)
+                const columna = document.getElementById(`columna${column.colCuatro}`)
                 columna.style.borderWidth = `${4}px`
                 columna.style.borderColor = `#ffff00`
             }else if(array1[i] != array2[i]){
+                
                 const columna = document.getElementById(`columna${i}`)
                 columna.style.borderWidth = `${5}px`
                 columna.style.borderColor = `#970f0f`
             }
+            
+            for( let i = 0 ; i<array1.length ; i++){
+                numerosComparacion.pop()
+                numerosComparacion.shift()
+            }
+        }
+
+
+
+
+
+        if (array1[0] === array2[0]) {
+            const columna = document.getElementById(`columna${column.colUno}`)
+            columna.style.borderWidth = `${6}px`
+            columna.style.borderColor = `#44ff00`         
+        }
+        if (array1[1] === array2[1]) {
+            const columna = document.getElementById(`columna${column.colDos}`)
+            columna.style.borderWidth = `${6}px`
+            columna.style.borderColor = `#44ff00`            
+        }
+        if (array1[2] === array2[2]) {
+            const columna = document.getElementById(`columna${column.colTres}`)
+            columna.style.borderWidth = `${6}px`
+            columna.style.borderColor = `#44ff00`            
+        }
+        if (array1[3] === array2[3]) {
+            const columna = document.getElementById(`columna${column.colCuatro}`)
+            columna.style.borderWidth = `${6}px`
+            columna.style.borderColor = `#44ff00`        
         }
         if (((array1[0] === array2[0]) && (array1[1] === array2[1])) && ((array1[2] === array2[2]) && (array1[3] === array2[3]))) {
             console.log('perfecto')
         }
+
+
+
+    contadorFila.suma1()
+    contadorFila.suma2()
+    selectorDeColumna.suma1()
+    selectorDeColumna.suma2()
+    selectorDeColumna.suma3()
+    selectorDeColumna.suma4()
+
     }
     else {
 
         console.log('por favor rellene los colores')
     }
+    
 }
