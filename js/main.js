8031bf8c7b225c51c81d185935e7fe907d8ba22f
Rumanito12 <ady_9697@yahoo.com>
2023-10-24 11:08:56 +0200

    arreglo de bugs y texto comentado

--- js/main.js
+++ js/main.js
@@ -7,15 +7,19 @@ const paletaDeColores = [];
 const arrayNumerosAleatorios = [];
 let numerosComparacion = [];
 
-( (colores) => {
+((colores) => {
     for (let i = 0; i < colores; i++) {
         paletaDeColores[i] = sessionStorage.getItem("color" + i)
     }
 })(nrDeColores)
 
-//Asignacion de datos en la web
+//Asignacion de datos en la web nombre del jugador y nivel de dificultad
+
 document.querySelector('.nombreJugador').innerHTML = nombreJugador
 document.querySelector('.nombreDificutad').innerHTML = `Level : ${dificultad}`
+
+// aqui se crean los divs selectores con los colores seleccionados..
+
 const creadorDeDivsInteractivos = (vueltas) => {
     for (let i = 0; i < vueltas; i++) {
         document.querySelector(".incorporacionDeDivsInteractivos").innerHTML += `
@@ -30,6 +34,8 @@ const creadorDeDivsInteractivos = (vueltas) => {
 
 creadorDeDivsInteractivos(nrDeColores)
 
+//con un bucle sacamos los colores guardados en sesion storage y los almacenamos en un array de colores
+
 const asignarColoresDeLaPaleta = (color) => {
     for (let i = 0; i < color.length; i++) {
         document.getElementById(`divInteractivo${i}`).style.backgroundColor = color[i]
@@ -37,6 +43,11 @@ const asignarColoresDeLaPaleta = (color) => {
 }
 
 asignarColoresDeLaPaleta(paletaDeColores)
+
+/*creacion de los divs donde se completa el juego segun la dificultad (filas y columnas)
+    el if de abajo representa el espaciado entre filas..
+*/
+
 const creadorDeDivsAsignados = (vueltas) => {
     for (let i = 0; i < vueltas; i++) {
         document.querySelector(".creadorDeDivsAsignados").innerHTML += `
@@ -46,7 +57,7 @@ const creadorDeDivsAsignados = (vueltas) => {
                                         </div>
                                     </div>
                                 </div>`
-        if ((i == 3) || (i == 7) || (i == 11) || (i == 15) || (i == 19) || (i == 23) || (i == 27) || (i == 31) || (i == 35)) {
+        if ((i%4===3)) {
             document.querySelector(".creadorDeDivsAsignados").innerHTML += `
                                     <div class="col-12 espaciadorPequenio"></div>`
         }
@@ -57,19 +68,16 @@ creadorDeDivsAsignados(nrDeIntentos * 4)
 
 //formación aleatoria de Numeros
 
-const numerosAleatorios = (num, colores) => {
-
-    let numeros = [];
-    for (let a = 0; a < num; a++) {
-        numeros.push(a)
-    }
-    numeros = numeros.sort(function () { return Math.random() - 0.5 })
+const numerosAleatorios = (num, colores) => {    
     for (let i = 0; i <= 3; i++) {
-        arrayNumerosAleatorios[i] = colores[numeros[i]]
-    }
+        arrayNumerosAleatorios[i] = colores[Math.floor(Math.random()*num)]
+}
 }
 
 numerosAleatorios(nrDeColores, paletaDeColores)
+
+                // PROGRAMACIÓN ORIENTADA A OBJTOS
+//ContadorColumna es el numero que representa el div por el cual se va rellenando segun elije los colores.
 
 class ContadorColumna {
     valor = 0;
@@ -83,6 +91,8 @@ class ContadorColumna {
         this.valor--;
     }
 }
+
+//ContadorFila es un contador de cada fila, se utiliza sobre todo  para saltos de una fila a otra
 
 class ContadorFila {
     min = 0;
@@ -98,6 +108,9 @@ class ContadorFila {
         this.max = this.max + 4
     }
 }
+
+//SelectorColumnaComprobacion son los numeros asignados a cada div, y en cada vuelta se van sumando +4
+// se utilizan en la parte de comprobación en los bucles "for"
 
 class SelectorDeColumnaComprobacion {
     colUno = 0;
@@ -124,6 +137,8 @@ class SelectorDeColumnaComprobacion {
     }
 }
 
+//PosicionArray se utiliza en asignar o borrar los colores de las filas
+
 class PosicionArray {
     posicion = 0;
     constructor() {
@@ -140,10 +155,14 @@ class PosicionArray {
     }
 }
 
-let contadorColumna = new ContadorColumna()
-let contadorFila = new ContadorFila()
-let selectorDeColumna = new SelectorDeColumnaComprobacion()
-let posicionArr = new PosicionArray()
+//se asignan todas las clases a una variable.
+
+const contadorColumna = new ContadorColumna()
+const contadorFila = new ContadorFila()
+const selectorDeColumna = new SelectorDeColumnaComprobacion()
+const posicionArr = new PosicionArray()
+
+//Aqui se asigna el color seleccionado en los divs selectores con el color corespondiente
 
 const selector = (num, numContador, fila) => {
     if ((numContador.valor >= 0) && (numContador.valor < fila.max)) {
@@ -153,6 +172,8 @@ const selector = (num, numContador, fila) => {
     }
 }
 
+//Aqui se borra los colores asignados de uno en uno
+
 const borrar = (numContador) => {
     if ((numContador.valor >= 1) && (numContador.valor > contadorFila.min)) {
         nr = 'numeroOculto'
@@ -161,6 +182,8 @@ const borrar = (numContador) => {
         ejecucion(nr, numContador.valor, contadorFila.max)
     }
 }
+
+//Aqui se ejecutan las 2 funciones de antes asignadas , segun se interactua con una o con otra
 
 const ejecucion = (num, valor, fila) => {
     if ((numerosComparacion.length <= fila) || (num == 'numeroOculto')) {
@@ -178,10 +201,23 @@ const ejecucion = (num, valor, fila) => {
     }
 }
 
+/* La parte mas Dificil la comprobación de los 2 arrays segun filas 
+    esta echo unpoco mas complicado de lo que podria estar
+    tiene la comparación de 2 arrays ( array1 "seleccion del jugador" y array2 "colores generados automaticamente")
+*/
+
 const verificacion = (array1, array2, column) => {
     if ((array1.length === 4) && (array1[0] != '')) {
         for (let i = 0; i <= array2.length; i++) {
+
+            //primero se comprueba si no son iguales, si son iguales se pasa al else que es ganado
+            //seguramente haya una forma mas facil de hacerlo
+
             if (array1[i] != array2[i]) {
+
+                //aqui compruebo cada "i" para asignar el color si se encuentra en la array
+                // ya que anteriormente se ha comprobado que las posiciones no coinciden
+
                 if ((i == 0)) {
                     const columna = document.getElementById(`columna${column.colUno}`)
                     columna.style.borderWidth = `${4}px`
@@ -202,6 +238,9 @@ const verificacion = (array1, array2, column) => {
                     columna.style.borderWidth = `${4}px`
                     columna.style.borderColor = `#ffff00`
                 }
+
+                //Si no se encuentra en la array se marcan en rojo
+
                 if (((array1[i] != array2[0]) && (array1[i] != array2[1])) && ((array1[i] != array2[2]) && (array1[i] != array2[3]))) {
                     if (i == 0) {
                         const columna = document.getElementById(`columna${column.colUno}`)
@@ -225,6 +264,9 @@ const verificacion = (array1, array2, column) => {
                     }
                 }
             } else {
+
+                //primero se comprueba si las 2 arrays coinciden las posiciones
+
                 if (array1[0] === array2[0]) {
                     const columna = document.getElementById(`columna${column.colUno}`)
                     columna.style.borderWidth = `${6}px`
@@ -245,12 +287,20 @@ const verificacion = (array1, array2, column) => {
                     columna.style.borderWidth = `${6}px`
                     columna.style.borderColor = `#44ff00`
                 }
+
+                // si todo coincide has ganado
+
                 if (((array1[0] === array2[0]) && (array1[1] === array2[1])) && ((array1[2] === array2[2]) && (array1[3] === array2[3]))) {
                     window.location.href = "ganador.html"
                     sessionStorage.setItem("ganador","HAS GANADO")
+                }else{
+                    perdido(array1,array2)
                 }
             }
         }
+
+        //Como es la parte de comprobacion tiene que pasar a la siguente fila, sumando filas, reiniciando la posicion de la array
+        // , sumando las columnas y reseteando la array de seleccion.
         contadorFila.suma1()
         contadorFila.suma2()
         posicionArr.reinicio()
@@ -260,10 +310,17 @@ const verificacion = (array1, array2, column) => {
         selectorDeColumna.suma4()
         numerosComparacion[posicionArr.posicion]=''
     }
+
+    //en caso de que no se elija un color se mande un console.log (para no poner un alert!!!!)
     else {
         console.log('por favor rellene los colores')
     }
-    if(contadorColumna.valor===nrDeIntentos*4){
+}
+
+    // en caso de no haber adivinado todos los colores pierdes, lo tuve que hacer de tal manera que si en la ultima casilla adivinas que te pase ganador
+
+const perdido = (array1,array2) =>{
+if((contadorColumna.valor===nrDeIntentos*4)&&((array1[0]!=array2[0])||(array1[1]!=array2[1])||(array1[2]!=array2[2])||(array1[3]!=array2[3]))){
         sessionStorage.setItem("ganador","Has Perdido")
         window.location.href = "ganador.html"
     }
--- js/nombre-dificultad.js
+++ js/nombre-dificultad.js
@@ -6,9 +6,16 @@
     
 const selectorDeDificultad = (colores,intentos,numeroColumna,dificultad) =>{
     const nombreJgador = document.getElementById("inputName").value
+    
         sessionStorage.setItem("nombreJugador",nombreJgador)
         sessionStorage.setItem("colores",colores)
         sessionStorage.setItem("intentos",intentos)
         sessionStorage.setItem("numColumna",numeroColumna)
         sessionStorage.setItem("dificultad",dificultad)
+        if(nombreJgador.length>1){
+            window.location.href = "selectorDeColores.html"
+        }else{
+            alert('introduce un nombre')
+        }
 }
+
